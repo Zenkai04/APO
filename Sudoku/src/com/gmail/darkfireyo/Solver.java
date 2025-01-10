5e6959c39ec57a324b20b5c385b9141582e3f693
Amin - Ayoub <ayoub.nadir@etu.univlyon1.fr>
2025-01-10 16:56:27 +0100

    update Solver v0.5

--- Sudoku/src/com/gmail/darkfireyo/Solver.java
+++ Sudoku/src/com/gmail/darkfireyo/Solver.java
@@ -1,5 +1,22 @@
 package com.gmail.darkfireyo;
 
 public class Solver {
+    private Grid grid;
     
+    public Solver(Grid grid) {
+        this.grid = grid;
+    }
+
+    public void useDeductionRule(){
+        int length = grid.getWidth();
+        for (int x = 0; x < length; x++) {
+            int valTest = grid.getElement(x, 0);
+            for (int y = 1; y < length; y++) {
+                if (valTest == grid.getElement(x, y)) {
+                    grid.setElement(x, y, valTest + 1);
+                    x = 0;
+                }
+            }
+        }
+    }
 }
