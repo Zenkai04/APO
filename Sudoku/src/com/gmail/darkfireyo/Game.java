6ee88577cc4628e9c54a6681c8b463422dd60398
AbderrahimBenMoha <benmoha.abdelbm@gmail.com>
2025-01-10 15:07:57 +0100

    class player + game version 1.0

---
+++ Sudoku/src/com/gmail/darkfireyo/Game.java
@@ -0,0 +1,28 @@
+public class Game {
+    private Solver solver;
+    private Generator generator;
+    private Grid currentGrid;
+
+    public Game(Solver solver, Generator generator) {
+        this.solver = solver;
+        this.generator = generator;
+        this.currentGrid = new Grid(9);
+    }
+
+    public void newGame() {
+        this.currentGrid = generator.generateCompleteGrid();
+    }
+
+    public void solveCurrentGrid() {
+        solver.solve(currentGrid);
+    }
+
+    public void generateNewPuzzle() {
+        this.currentGrid = generator.generateCompleteGrid();
+        generator.removeNumbers(currentGrid);
+    }
+
+    public Grid getCurrentGrid() {
+        return currentGrid;
+    }
+}
---
+++ Sudoku/src/com/gmail/darkfireyo/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+    private String name;
+    private Game currentGame;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void createNewGame(Solver solver, Generator generator) {
+        this.currentGame = new Game(solver, generator);
+    }
+
+    public void solveGame() {
+        if (currentGame != null) {
+            currentGame.solveCurrentGrid();
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Game getCurrentGame() {
+        return currentGame;
+    }
+}
